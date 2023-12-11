c45cecbf734832c00d97fc3dcac491c1578b2dae
xanthe955 <84999118+xanthe955@users.noreply.github.com>
2023-12-11 13:06:00 -0600

    Update views.py

--- productos/views.py
+++ productos/views.py
@@ -440,7 +440,7 @@ def t3pe_sell(request):
             }
 
             # Enviar datos a FastAPI
-            main_api_url = "https://tear3prueba.azurewebsites.net/clientrequest/"
+            main_api_url = "https://tear3prueba.azurewebsites.net/clientrequest"
             response = requests.post(main_api_url, json=data)
 
             if response.status_code == 200:
